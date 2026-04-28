bcf03c2523f1e38b70faf6d1e6851c9c9b6fbbb0
Serikbaev Zhubanysh <serikbaevzhubanysh2007@gmail.com>
2026-04-28 23:19:49 +0500

    hehhehhehe

---
+++ tsis/1tsis/config.py
@@ -0,0 +1,13 @@
+# import psycopg2
+# params = {
+#     "host": "127.0.0.1",
+#     "user": "postgres",
+#     "password": "8706",
+#     "port": 5432,
+#     "dbname": "PhoneBook"
+# }
+DB_NAME = "PhoneBook"
+DB_USER = "postgres"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_PASS = "8706" 
